e49ba374b0ab9f919b5d6c1cf140d2ae364e9deb
chihunmanse <chihunmanse@gmail.com>
2022-01-11 18:28:34 +0900

    wishlist

---
+++ api/wishlist/services/error.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const errorHandle = {
+  PRODUCT_NOT_FOUND: {
+    id: "Product.not.found",
+    statusCode: 404,
+    message: "Product does not exist",
+  },
+};
+
+const errorHandler = (key) =>
+  strapi.services.common.errorHandlerV3("Wishlist", errorHandle, key);
+
+module.exports = { errorHandler };
